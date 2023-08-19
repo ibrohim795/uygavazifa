c31c68a72a13ea1d7a6df1eb0993afad8ca4118f
ibrohim795 <iismatov59@gmail.com>
2023-08-19 15:23:12 +0500

    bajarildi

---
+++ uygavazifa/uygavazifa/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygavazifa
+{
+    internal class Car
+    {
+        public Car(decimal id , string model , string brand, decimal price , decimal taxPercentage)
+        {
+            Id = id;
+            Model = model;
+            Brand = brand;
+            Price = price;
+            TaxPercentage = taxPercentage;
+
+        }
+        public decimal Id { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public static decimal TaxPercentage { get; set; }
+        public  decimal CalculaTetotal()
+        {
+            return Price-(Price*TaxPercentage/100);
+        }
+    }
+}
---
+++ uygavazifa/uygavazifa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace uygavazifa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(777,"CHevrolet","Onix",100,15);
+            Console.WriteLine( car.CalculaTetotal());
+            Car.TaxPercentage = 50;
+            Console.WriteLine(car.CalculaTetotal());
+        }
+    }
+}
